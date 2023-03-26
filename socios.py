f4ce81594aac39088c2add0b42a4fbc30d7dfcba
JURANDI <60081684+jurandi82@users.noreply.github.com>
2023-03-26 12:53:16 -0300

    atualização para pesquisar os nomes da empresa

--- socios.py
+++ socios.py
@@ -7,13 +7,19 @@ from tqdm.notebook import tqdm
 
 class socios():
     G = nx.Graph()
+    __full = False
+    __db = None
 
     def __init__(self,db,test=True):
-        engine = sa.create_engine(db)
+        socios.__db = db
+        engine = sa.create_engine(socios.__db)
         #engine = sa.create_engine('sqlite:///grapho20230315.db')
+        #engine = sa.create_engine('sqlite:///base_receita20230315.db')
     
         print("Lendo database...")
         with engine.connect() as db:
+            query="SELECT name FROM sqlite_schema WHERE type ='table' AND name='EMPRESA' limit 1;"
+            socios.__full = True if db.execute(sa.text(query)).fetchone() else False
             query ='''SELECT COUNT() FROM grapho;'''
             total = db.execute(sa.text(query)).fetchall()[0][0]
             if test:
@@ -25,11 +31,11 @@ class socios():
                 socios.G.add_edge(*i)
         print("Número de registros carregados: {:,}".format(socios.G.number_of_nodes()).replace(",","."))
 
-        lb_alvo = wd.Label("ALVO:")
-        tx_alvo = wd.Text(value='TELEFONICA')
+        lb_alvo = wd.Label("DESTINO:")
+        tx_alvo = wd.Text(value='00000000')
         box_alvo=wd.HBox([lb_alvo,tx_alvo])
         lb_pesquisa = wd.Label("ORIGEM:")
-        tx_pesquisa = wd.Text(value='NESTLE')
+        tx_pesquisa = wd.Text(value='43073394')
         bt_pesquisa = wd.Button(description="PESQUISAR", icon='search')
         out = wd.Output(layout={'border': '1px solid black'})
         box_origem=wd.HBox([lb_pesquisa,tx_pesquisa,bt_pesquisa])
@@ -42,7 +48,7 @@ class socios():
 
         bt_pesquisa.on_click(executa)
 
-        print("Sugestoes de pesquisa: Nestle, Telefonica,Odebrecht,Wassef...\n")
+        print("Sugestoes de pesquisa: Entre com Radical do CNPJ com 8 digitos ou Nome completo \n")
         display(box)
 
     def validar(pesquisa:str)->str:
@@ -52,15 +58,30 @@ class socios():
                 return node
         return ""
 
+    def razao(lista:list)->list:
+        if socios.__full:
+            engine = sa.create_engine(socios.__db)
+            res=[]
+            with engine.connect() as db:
+                for i in lista:
+                    c=""
+                    if len(i)==8:
+                        c = db.execute(sa.text(f"SELECT RAZAO_SOCIAL FROM EMPRESA WHERE CNPJ_BASICO={i} LIMIT 1;")).fetchone()[0]
+                    res.append(i+" "+c)
+        else:
+            return lista
+        return res
+
     def localizar(inicio, fim):
         inicio, fim = socios.validar(inicio), socios.validar(fim)
-        print(f" Inicio validado: {inicio} \n Fim validado {fim}\n")
+        print(f" Destino validado: {inicio} \n Origem validado {fim}\n")
         if inicio !="" and fim !="":
             if not nx.has_path(socios.G,inicio ,fim):
                 print("Caminho não encontrado")
             else:
                 print("Caminho localizado \n"+"*"*64)
-                print(*nx.shortest_path(socios.G,source=fim,target=inicio),sep = "\n")
+                path=nx.shortest_path(socios.G,source=fim,target=inicio)
+                print(*socios.razao(path),sep = "\n")
             print("\n")
 
 
